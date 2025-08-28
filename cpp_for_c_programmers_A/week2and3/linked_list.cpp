ff852da9477a55b2d875d91d21e81fc1d30f2dae
Sounak Biswas <sounak.pablo@gmail.com>
2025-08-28 15:24:07 +0200

    prints etc added

--- cpp_for_c_programmers_A/week2and3/linked_list.cpp
+++ cpp_for_c_programmers_A/week2and3/linked_list.cpp
@@ -3,43 +3,74 @@ using namespace std;
 template <class T>
 class listelem
 { 
+  public:
   T data;
   listelem * next;
 
   listelem(T x, listelem* nx=0): data(x), next(nx){}
+  ~listelem(){
+    cout<<"destructor called for element,"<<"data="<<this->data<<endl;
+    delete next;
+  };
+
 };
 
 template <class T>
 class slist{
-  T* head;
-  slist(T init){
-    this->head = listelem<T>(init,0);
-  }
   public :
+    listelem<T>* head;
+    slist(T init){
+      this->head = new listelem<T>(init,0);
+    }
     void append(T x);
     void prepend(T x);
     ~slist();
 };
+
+template <class T>
+slist<T>::~slist(){
+  cout<<"list destructor called"<<endl;
+  listelem<T>* it=this->head;
+  listelem<T>* temp;
+  while (it!=NULL) {
+    temp=it;
+    it=it->next;
+  }
+  
+}
+
+
+
+
 template <class T>
 void  slist<T>::append(T x) {
-    T* ptr= head;
+    listelem<T>* ptr= head;
     while( ptr->next != NULL)
       ptr = ptr->next;
-    ptr->next=listelem<T>(x,0);
+    ptr->next=new listelem<T>(x,0);
   };
 
 template <class T>
   void slist<T>:: prepend(T x) {
-    listelem<T> * temp = listelem<T>(x,this->head);
+    listelem<T> * temp = new listelem<T>(x,this->head);
     this->head = temp;
   };
 template <class T>
-slist<T>::~slist(){
-  listelem<T>* it=this->head;
-  listelem<T>* temp;
-  while (it!=NULL) 
-    it=it->next;
-  
+  ostream& operator<<(ostream& os,slist<T>& list) {
+    cout<<"print list"<<endl;
+    listelem<T>* ptr= list.head;
+    while( ptr->next != NULL){
+      os<<ptr->data<<" ";
+      ptr = ptr->next;
+    }
+    return os;
+  };
+int main(){
+  slist<int> arr(1);
+  for (int i=1; i<= 10; i++)
+    arr.append(i);
+  arr.prepend(-1);
+  cout<<arr<<endl;
 }
 
 
