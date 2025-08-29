69dfe24cc197fdb28ea7d0e68377faf56a8e5e06
Sounak Biswas <sounak.pablo@gmail.com>
2025-08-29 21:59:45 +0200

    debugged

--- cpp_for_c_programmers_A/week2and3/dijkstra_hw2.cpp
+++ cpp_for_c_programmers_A/week2and3/dijkstra_hw2.cpp
@@ -1,7 +1,7 @@
+/* Dijkstra with naive implementation of PQ */
 #include <iostream>
-#include <stack>
-#include <vector>
 #include <limits>
+#include <ctime>
 using namespace std;
 /* Class graph. I maintain a edgelist instead of 
  * adjacency matrix, hoping it would be faster for not-too-dense graphs.
@@ -33,15 +33,38 @@ class graph {
         n_edges[v2]++;
         this->cost[v1][v2]=this->cost[v2][v1]=cost;
     }
+    void remove_edge(int v1,int v2){
+        for(int edge=0; edge<n_edges[v1]; edge++){
+            if (edge_list[v1][edge]==v2){
+                swap(edge_list[v1][edge],edge_list[v1][n_edges[v1]-1]);
+                n_edges[v1]--;
+            }
+
+        }
+    }
+    void clear_edges(){
+        for(int vert=0; vert<n_verts; vert++){
+            n_edges[vert]=0;
+        }
+    }
 
     graph(int n_verts=10){
         this->n_verts=n_verts;
-         
+
         n_edges= new int[n_verts]();
         edge_list=new int *[n_verts];
+        cost=new double *[n_verts];
         for(int i=0; i <n_verts; i++) {
             edge_list[i]= new int[n_verts]();
+            cost[i]= new double[n_verts]();
         }
+    }
+    int tot_edges(){
+        int tot_edges=0;
+        for(int i =0; i<n_verts; i++){
+            tot_edges+=n_edges[i];
+        }
+        return tot_edges/2;
     }
 
     ~graph(){
@@ -53,8 +76,20 @@ class graph {
         delete[] cost;
     }
     void shortest_path_dijkstra(int v1,double[] );
-
+    void add_random_edges(double density,double , double);
 };
+void graph::add_random_edges(double density,double mindist=1.0, double maxdist=1.0){
+    double rand_dist;
+    for(int vert1=0; vert1<n_verts; vert1++){
+        for(int vert2=vert1+1; vert2<n_verts; vert2++){
+            rand_dist=mindist+(rand()/(RAND_MAX+1.0))*(maxdist-mindist);
+            //cout<<rand_dist<<endl;
+            if (rand()/(RAND_MAX+1.0)<density)
+                this->add_edge(vert1,vert2);
+        }
+    }
+}
+
 /* Class priority_queue
  * This implements the Open set from the lectures in a stack-like structure called 
  * priority queue, of which I have made a naive implementation.
@@ -80,71 +115,121 @@ class graph {
  *
  */
 class priority_queue{
+    public:
     int top;
     int* elements;
-    const int nmax;
     const double *priority;
-    public:
-       priority_queue(int nmax,double* priority):nmax(nmax), priority(priority){
-           elements=new int[nmax];
+    const int nmax;
+    priority_queue(int nmax,double* priority):nmax(nmax), priority(priority){
+        top=0;
+        elements=new int[nmax];
 
-       }
-       ~priority_queue(){
-           delete[] elements;
-       }
-       int pop(){
-           double min=static_cast<double>(nmax);
-           int min_elem=0;
-           for(int i=0; i<top; i++){
-               if(priority[elements[i]]<min)
-                   min_elem=i;
-           }
-           //pop the minimum element from the priority_queue
-           swap(elements[top-1],elements[min_elem]);
-           return elements[top--];
-       }
-       void add_element(int vert){
-           elements[top++]=vert;
-       }
-       bool if_empty(){
-           return (top==0);
-       }
-       
+    }
+    ~priority_queue(){
+        delete[] elements;
+    }
+    int pop(){
+        double min= numeric_limits<double>::infinity();
+        int min_elem=0;
+        for(int i=0; i<top; i++){
+            if(priority[elements[i]]<min)
+                min_elem=i;
+        }
+        //pop the minimum element from the priority_queue
+        swap(elements[top-1],elements[min_elem]);
+        return elements[--top];
+    }
+    void add_element(int vert){
+        //cout<<"add elem, top:"<<top<<" vert:"<<vert<<" prior:"<<priority[vert]<<endl;
+        //getchar();
+        elements[top++]=vert;
+    }
+    bool if_empty(){
+        return (top==0);
+    }
+    bool contains_element(int v){
+        for(int i=0; i<top; i++)
+            if(elements[i]==v)
+                return true;
+        return false;
+    }
+
 };
 
 
 /*dijkstra's algorithm
  *
- * Dijkstra's algorithm as in lectures. Each time the vertex with minimum distance 
- * is removed from the open set.
+ * Dijkstra's algorithm as in lectures. The open set from lectures is implemented as a 
+ * priority queue (PQ). The PQ is starts off with all vertices reachable from the source node. 
+ * The nearest node is removed from 
  */
+
 
 void graph::shortest_path_dijkstra(int src, double dist[] ){
     double present_dist;
+    bool *closed = new bool[n_verts];
     int current_node;
-    for(int vert=0; vert<n_verts; vert++)
+    for(int vert=0; vert<n_verts; vert++){
         dist[vert]=numeric_limits<double>::infinity();
+        closed[vert]=false;
+    }
 
     priority_queue open_set(n_verts,dist);
+    //cout<<open_set.nmax<<endl;
     dist[src]=0;
 
     open_set.add_element(src);
 
     while(not open_set.if_empty()){
+        //cout<<"bef nelem:"<<open_set.top<<endl;
         current_node=open_set.pop();
+        //cout<<"popped:"<<current_node<<endl;
+        //cout<<"aft nelem:"<<open_set.top<<endl;
+        closed[current_node]=true;
         for(int edge=0; edge<n_edges[current_node]; edge++){
             int nbr=edge_list[current_node][edge];
-            double newdist=dist[current_node]+cost[current_node][nbr];
-            if(newdist<dist[nbr]){
-                dist[nbr]=newdist;
+            if(! closed[nbr]){
+                if(! open_set.contains_element(nbr)) 
+                    open_set.add_element(nbr);
+                double newdist=dist[current_node]+cost[current_node][nbr];
+                //cout<<"dist change:"<<dist[nbr]<<" "<<newdist<<endl;
+                if(newdist<dist[nbr]){
+                    dist[nbr]=newdist;
+
+                }
             }
         }
-
     }
-
-
 }
 
+int main(){
+    int n_verts=50;
+    double densities[]={0.2,0.4};
+    double density;
+    double *dist=new double[50];
+    const double min_dist=1.0;
+    const double max_dist=10.0;
+    const int ngraphs=100;
+    graph G(n_verts);
+    srand(clock());
 
+    for(int density_ix=0; density_ix<2; density_ix++){
+        double avg_path=0;
+        int pair_count=0;
+        density=densities[density_ix];
+        for(int ngr=0; ngr<ngraphs; ngr++){
+           G.clear_edges();
+           G.add_random_edges(density,1.0,10.0);
+           G.shortest_path_dijkstra(0,dist);
+           for(int i=1; i <n_verts; i++){
+               if(dist[i]!=numeric_limits<double>::infinity()){
+                   pair_count+=1;
+                   avg_path+=dist[i];
+               }
+           }
 
-int main(){}
+        }
+        cout<<"density"<<density<<", average shortest path="<<avg_path/(ngraphs*(n_verts-1.0))<<endl;
+    }
+}
+
